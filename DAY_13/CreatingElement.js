69ce4b8b656c69d837f3f058c9492f0a64d7a099
shraddha goyal <shraddhagoyal800@gmail.com>
2024-02-04 19:35:36 +0530

    innerhtml, text content

--- DAY_13/CreatingElement.js
+++ DAY_13/CreatingElement.js
@@ -12,9 +12,30 @@ division.appendChild(btn);
 
 const para = document.createElement("p");
 para.textContent = "Shraddha you are good girl";
-document.querySelector('div').appendChild(para)
+document.querySelector('div').appendChild(para) 
 
 const div = document.createElement("div");
 div.className = "divison";
 
 document.querySelector("body").appendChild(div)
+
+const first = document.getElementById("para");
+console.log(first);
+const element = first.innerHTML;
+const element1 = first.textContent;
+first.innerHTML = "<strong> Hello Moto </strong> <p> heeloo</p>" // the text will be overwitted
+first.textContent = "Hii this is krishna" // the text is overwrited
+console.log(element);
+console.log(element1);
+
+
+// using backtic
+const ele = `
+    <div>Element 1</div>
+    <div>Element 2</div>
+    <div>Element 3</div>
+`
+// console.log(document.getElementsById("second1"))
+const id1 = document.getElementById("second1")
+id1.innerHTML = ele;
+// document.getElementsById("second1").innerHTML = ele;
