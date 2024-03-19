b1585dd7a8264b71c758a3cfe53ac1b32d35c041
shraddha goyal <shraddhagoyal800@gmail.com>
2024-03-19 18:26:02 +0530

    Completed the bubble game

--- MINI_PROJECTS/PROJECT_6/index.js
+++ MINI_PROJECTS/PROJECT_6/index.js
@@ -1,14 +1,15 @@
 const pbtm = document.querySelector("#pbtm");
 const timer = document.querySelector("#timer");
 const hit = document.querySelector("#hit");
+const scoreValue = document.querySelector("#scoreValue");
 
 let i = 60;
-let hitNum = 60;
+let score = 0;
+let num;
 
 // The function to make the bubble
 function makeBubble() {
   let bubbleAdd = "";
-
   for (let i = 1; i <= 120; i++) {
     let num = Math.floor(Math.random() * 10);
     bubbleAdd += `<div class="bubble">${num}</div>`;
@@ -17,24 +18,52 @@ function makeBubble() {
   pbtm.innerHTML = bubbleAdd;
 }
 
+// Function to run the timer
 function runTimer() {
-  if (i < 0) return;
-  setInterval(() => {
-    timer.textContent = i;
-    i--;
+  let timerInv = setInterval(() => {
+    if (i >= 0) {
+      timer.textContent = i;
+      i--;
+    } else {
+      clearInterval(timerInv);
+      pbtm.innerHTML = "<h1>Game Over</h1>";
+    }
   }, 1000);
 }
 
+
+// function to change the value of hit funtion
 function hitFun() {
-  if (hitNum < 0) return;
-  setInterval(() => {
-    let num = Math.floor(Math.random() * 10);
-    hit.textContent = num;
-    hit.textContent = num;
-    hitNum--;
-  }, 1000);
+  num = Math.floor(Math.random() * 10);
+  hit.textContent = num;
 }
+
+// function to increase the score
+function scoreIncrease() {
+  score += 10;
+  scoreValue.textContent = score;
+}
+
+
+// Function to calculate the results
+function getResults() {
+  pbtm.addEventListener("click", (e) => {
+    let val = Number(e.target.textContent);
+    console.log(val);
+    if (num === val) {
+      scoreIncrease();
+      hitFun();
+      makeBubble();
+    } else {
+      alert("OOPS!! Wrong Number clicked..");
+      hitFun();
+      makeBubble();
+    }
+  });
+}
+
 
 makeBubble();
 runTimer();
 hitFun();
+getResults();
